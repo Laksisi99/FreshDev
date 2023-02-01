b1013b35181988fba27fad8ec4c2f1150a7d5e9e
Laksisi Chetha Gunaratne <90137011+Laksisi99@users.noreply.github.com>
2023-02-01 16:25:07 +0530

    other page done
    other page done

--- freshdev-app/src/pages/Other.js
+++ freshdev-app/src/pages/Other.js
@@ -2,9 +2,34 @@ import React from 'react'
 
 const Other = () => {
   return (
-    <div>
-      Other
+    <main>
+    <div className='pg-header'>
+      <div className='container'>
+        <h1>Other Paths For you</h1>
+      </div>
     </div>
+    <div className='container-content'>
+          <h5>In addition to developing, the fields that can be engaged in IT.</h5>
+        
+            <h6>💻Computer support</h6>
+
+            <p>Many people in the early stages of their IT careers work in computer support roles, which support a wide range of computer operations. From there, you can specialize in a narrower field, such as cybersecurity or networks, or progress in computer support to become a senior or manager.</p>
+            
+            <h6>💻Cybersecurity</h6>
+
+            <p>Working in cybersecurity entails safeguarding computer systems, devices, and sensitive data against malware, unauthorized access, damage, and data breaches. According to Global Knowledge, an IT professional development company, cybersecurity is one of the two IT fields that will see the most demand.</p>
+            
+            <h6>💻Data Engineer</h6>
+
+            <p>Because of the pervasiveness of internet-connected devices, businesses now have unprecedented access to data on their customers, services, and other business factors. And, while data isn't a traditional IT specialization, the increased demand for data skills makes it a viable career path for those seeking IT-related work. Working with data can entail anything from maintaining hardware and security to sifting through data to find patterns and insights as a data analyst or scientist.</p>
+            
+            <h6>💻Cloud Engineer</h6>
+
+            <p>Cloud computing, along with cybersecurity, was identified by Global Knowledge as the top technical area in IT experiencing the greatest demand. A job in cloud technology typically entails some programming on cloud software platforms such as Azure, AWS, or Google Cloud.</p>
+        
+          
+    </div>
+  </main>
   )
 }
 
